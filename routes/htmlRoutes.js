14027d1d8f209fdee601d3cec27f37079dd7ddcd
Derek Stoeckmann <d.stoeckmann@gmail.com>
2019-12-03 12:49:11 -0700

    add bootstrap css layout

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,20 @@
 var db = require("../models");
 
 module.exports = function (app) {
-  app.get("/home", function (req, res) {
-    res.render("index");
+  app.get("/", async function (req, res) {
+    try {
+      const posts = await db.post.findAll(
+        {
+          raw: true,
+          include: db.user
+        }
+      );
+
+      res.render("index", { posts });
+    }
+    catch (err) {
+      console.log(err)
+      res.status(500).end();
+    };
   });
 };
