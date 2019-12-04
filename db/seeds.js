abd0762bae195dfedcad7e6d094970530c26756b
verlitas <55808608+verlitas@users.noreply.github.com>
2019-12-04 10:36:21 -0700

    updating schema and seeds

--- db/seeds.js
+++ db/seeds.js
@@ -1,86 +1,57 @@
 const db = require("../models");
 
 db.sequelize.sync({ force: true }).then(() => {
-    db.user.bulkCreate([
-        {
-            firstName: "TJ",
-            lastName: "Cheet",
-            username: "tjcheet",
-            bio: "I'm a cheetah"
-        },
-        {
-            firstName: "Melody",
-            lastName: "Kite",
-            username: "mkite",
-            bio: "I'm a shoebill"
-        },
-        {
-            firstName: "Kondali",
-            lastName: "Kangha",
-            username: "kondkan",
-            bio: "I'm a kangeroo"
-        },
-        {
-            firstName: "Derek",
-            lastName: "Shoe",
-            username: "dereshoe",
-            bio: "I'm a giraffe"
-        },
-        {
-            firstName: "Tucker",
-            lastName: "Champ",
-            username: "tchamp",
-            bio: "I'm a male duck"
-        }
-    ]);
-
     db.post.bulkCreate([
         {
             text: "This is a beautiful post from author 1",
-            userId: 1,
-        },
-        {
-            text: "Another beautiful post from author 1",
-            userId: 1,
-        },
-        {
-            text: "This is a beautiful post from author 2",
-            userId: 2
+            displayName: "tjlovesham"
         },
         {
             text: "Another beautiful post from author 2",
-            userId: 2
+            displayName: "dereklovesham"
         },
         {
             text: "This is a beautiful post from author 3",
-            userId: 3
-        },
-        {
-            text: "Another beautiful post from author 3",
-            userId: 3
-        },
-        {
-            text: "This is a beautiful post from author 4",
-            userId: 4
+            displayName: "melodylovesham"
         },
         {
             text: "Another beautiful post from author 4",
-            userId: 4
+            displayName: "tuckerlovesham"
         },
         {
             text: "This is a beautiful post from author 5",
-            userId: 5
+            displayName: "shellylovesham"
         },
         {
-            text: "Another beautiful post from author 5",
-            userId: 5
+            text: "Another beautiful post from author 6",
+            displayName: "beckylovesham"
+        },
+        {
+            text: "This is a beautiful post from author 7",
+            displayName: "kondalilovesham"
+        },
+        {
+            text: "Another beautiful post from author 8",
+            displayName: "leisalovesham"
+        },
+        {
+            text: "This is a beautiful post from author 9",
+            displayName: "chrislovesham"
+        },
+        {
+            text: "Another beautiful post from author 10"
         }
     ]);
 
     db.comment.bulkCreate([
         {
-            text: "this is a comment",
+            text: "this is a comment 1",
             postId: 1
+        },
+        {
+            text: "this is a comment 1",
+            displayName: "tjlovesham",
+            postId: 2
         }
     ])
 });
--- models/post.js
+++ models/post.js
@@ -9,16 +9,27 @@ module.exports = function (sequelize, DataTypes) {
         text: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        displayName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "Anonymous"
+        },
+        score: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        },
+        avatar: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        },
+        imageUrl: {
+            type: DataTypes.STRING,
+            allowNull: true
         }
     });
-    post.associate = function (models) {
-        models.post.belongsTo(models.user, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-
     post.associate = function (models) {
         models.post.hasMany(models.comment, {
             foreignKey: {
